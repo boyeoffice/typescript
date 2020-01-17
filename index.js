505e7cbeb34dac1df7751996989373fc93ed6963
Olakunle Boye <kunlexzy@gmail.com>
2020-01-17 20:04:56 +0100

    finished

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 async function hello() {
     return 'Hello world!';
 }
+let lucky;
+lucky = 23;
+let font;
+const person = {
+    first: 'Jeff',
+    last: 'Delaney'
+};
+const person2 = {
+    first: 'Usain',
+    last: 'Bolt',
+    fast: true
+};
+function pow(x, y) {
+    return Math.pow(x, y);
+}
+pow(5, 10);
+function pow2(x, y) {
+    return Math.pow(x, y).toString();
+}
+pow2(5, 10);
+const arr = [];
+arr.push(1);
+arr.push('123');
+arr.push(false);
+//Generics
+class Observable {
+    constructor(value) {
+        this.value = value;
+    }
+}
+let x;
+let y;
+let z = new Observable(23);
--- index.ts
+++ index.ts
@@ -1,3 +1,57 @@
+import * as _ from 'lodash'
+
 async function hello() {
 	return 'Hello world!'
 }
+let lucky: number
+lucky = 23
+
+//type Style = 'string';
+
+type Style = 'bold' | 'italic' | 23
+
+let font: Style;
+
+interface Person  {
+	first: string;
+	last: string;
+	[key: string]: any
+}
+const person: Person = {
+	first: 'Jeff',
+	last: 'Delaney'
+}
+
+const person2: Person = {
+	first: 'Usain',
+	last: 'Bolt',
+	fast: true
+}
+
+
+ function pow(x: number, y: number) {
+ 	return Math.pow(x, y);
+ }
+pow(5, 10)
+
+function pow2(x: number, y: number): string {
+ 	return Math.pow(x, y).toString();
+ }
+pow2(5, 10)
+
+// Arrays
+type MyList = [number?, string?, boolean?]
+const arr: MyList = []
+arr.push(1)
+arr.push('123')
+arr.push(false)
+
+//Generics
+class Observable<T> {
+	constructor(public value: T) {}
+}
+
+let x: Observable<number>;
+let y: Observable<Person>;
+let z = new Observable(23)
+
